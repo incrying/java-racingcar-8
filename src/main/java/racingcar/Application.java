d3e2c1f6394aaae81998517b19c55787252f25e9
인다운 <incrying@duksung.ac.kr>
2025-10-26 23:44:30 +0900

    feat(Application): 진입점 구현

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,24 @@
 package racingcar;
 
+import racingcar.config.AppConfig;
+import racingcar.controller.CarRaceGame;
+import racingcar.view.InputView;
+import racingcar.view.OutputView;
+
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        AppConfig appconfig = new AppConfig();
+
+        InputView inputView = appconfig.inputView();
+        List<String> input = inputView.readCarNames();
+        int count = inputView.readNumber();
+
+        CarRaceGame game = appconfig.carRaceGame();
+        String result = game.run(input,count);
+
+        OutputView outputView = appconfig.outputView();
+        outputView.printWinner(result);
     }
 }
